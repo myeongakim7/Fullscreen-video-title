f5e74c76701513b0eb2dbf793ab6a6ee2795af6b
myeongakim7 <113976428+myeongakim7@users.noreply.github.com>
2023-01-26 16:51:20 +0900

    video 태그와 속성을 활용한 풀 스크린 비디오 타이틀
    -video 태그와 속성을 활용
    -Play 버튼 클릭 시 동영상 재생
    -Pause 버튼 클릭 시 동영상 일시 중지
    -최소 너비, 높이로 해야 배경이 꽉 참

---
+++ main.js
@@ -0,0 +1,15 @@
+//video 요소 선택
+var vid = document.getElementById("bg");
+
+// play
+function playVid() {
+  vid.play();
+}
+// pause
+function pauseVid() {
+  vid.pause();
+}
+
+// // function 임의함수 정하기(){
+//   변수.내장객체()
+// }
